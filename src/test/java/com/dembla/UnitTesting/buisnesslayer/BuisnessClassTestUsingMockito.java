58a8b2e65c2a4afd66f588a0e5367c41da22b027
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-07-19 12:10:18 +0530

    Use Annotation BeforeEach (in case of Junit 4 it is before) to get it executed before the each testcase.

--- src/test/java/com/dembla/UnitTesting/buisnesslayer/BuisnessClassTestUsingMockito.java
+++ src/test/java/com/dembla/UnitTesting/buisnesslayer/BuisnessClassTestUsingMockito.java
@@ -2,6 +2,7 @@ package com.dembla.UnitTesting.buisnesslayer;
 
 import com.dembla.UnitTesting.services.DataStore;
 import org.hibernate.id.enhanced.DatabaseStructure;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.crypto.Data;
@@ -12,49 +13,33 @@ import static org.mockito.Mockito.when;
 
 public class BuisnessClassTestUsingMockito {
 
+    BuisnessClass buisnessClass = new BuisnessClass() ;
+    DataStore dataStore = mock(DataStore.class) ;
+
+    @BeforeEach
+    public void before(){
+        buisnessClass.setDataStore(dataStore);
+    }
+
     @Test
     public void calculateSumUsingMockito(){
 
-        BuisnessClass buisnessClass = new BuisnessClass() ;
-
-        DataStore dataStore = mock(DataStore.class) ;
         when(dataStore.retrieveData()).thenReturn(new  int[]{1,2,3,4}) ;
-
-        buisnessClass.setDataStore(dataStore);
-        int result = buisnessClass.calculateeSum() ;
-        int actualresult = 10 ;
-
-        assertEquals(actualresult,result);
+        assertEquals(10, buisnessClass.calculateeSum());
     }
 
     @Test
     public void calculateSumNoValue(){
 
-        BuisnessClass buisnessClass = new BuisnessClass() ;
-
-        DataStore dataaStore = mock(DataStore.class) ;
-        when(dataaStore.retrieveData()).thenReturn(new int[]{}) ;
-
-        buisnessClass.setDataStore(dataaStore) ;
-
-         int result = buisnessClass.calculateeSum() ;
-         int actualResult = 0 ;
-          assertEquals(actualResult,result);
+        when(dataStore.retrieveData()).thenReturn(new int[]{}) ;
+        assertEquals(0, buisnessClass.calculateeSum());
     }
 
     @Test
     public void calculateSumOneValue(){
 
-        BuisnessClass buisnessClass = new BuisnessClass() ;
-
-        DataStore dataaStore = mock(DataStore.class) ;
-        when(dataaStore.retrieveData()).thenReturn(new int[]{5}) ;
-
-        buisnessClass.setDataStore(dataaStore) ;
-
-        int result = buisnessClass.calculateeSum() ;
-        int actualResult = 5 ;
-        assertEquals(actualResult,result);
+        when(dataStore.retrieveData()).thenReturn(new int[]{5}) ;
+        assertEquals(5, buisnessClass.calculateeSum());
     }
 
 
