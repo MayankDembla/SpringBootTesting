4dae8fc76a65a2d8154c4ddaf9f27b40a83bf099
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-07-20 02:21:23 +0530

    Use Case when Method is Parametorized.

--- src/test/java/com/dembla/UnitTesting/buisnesslayer/ListMockTest.java
+++ src/test/java/com/dembla/UnitTesting/buisnesslayer/ListMockTest.java
@@ -1,31 +1,42 @@
 package com.dembla.UnitTesting.buisnesslayer;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ListMockTest {
 
 
+    @Mock
+    List mock ;
+
     @Test
     public void testbasic(){
-        List mock = Mockito.mock(List.class) ;
         when(mock.size()).thenReturn(5) ;
         assertEquals(5,mock.size()) ;
     }
 
     @Test
     public void testReturnMultipleValue(){
-        List mock = Mockito.mock(List.class) ;
         when(mock.size()).thenReturn(5).thenReturn(10) ;
         assertEquals(5,mock.size()) ;
         assertEquals(10, mock.size());
     }
 
+    @Test
+    public void returnWithParameter() {
+        when(mock.get(0)).thenReturn("mayank")  ;
+        when(mock.get(1)).thenReturn("dembla")  ;
+        assertEquals("mayank",mock.get(0));
+        assertEquals("dembla",mock.get(1));
+        assertEquals(null,mock.get(2));        // get default value.
+    }
 
 
 }
