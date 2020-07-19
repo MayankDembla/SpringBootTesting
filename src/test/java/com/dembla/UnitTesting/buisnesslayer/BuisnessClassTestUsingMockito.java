8fbf5b5152d426172eee7d0b939538050df98c91
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-07-20 01:44:45 +0530

    Use Annotation InjectMock and Mock , and @ExtendWith to get Mockito Extension Class.

--- src/test/java/com/dembla/UnitTesting/buisnesslayer/BuisnessClassTestUsingMockito.java
+++ src/test/java/com/dembla/UnitTesting/buisnesslayer/BuisnessClassTestUsingMockito.java
@@ -1,25 +1,28 @@
 package com.dembla.UnitTesting.buisnesslayer;
 
 import com.dembla.UnitTesting.services.DataStore;
-import org.hibernate.id.enhanced.DatabaseStructure;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import javax.xml.crypto.Data;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class BuisnessClassTestUsingMockito {
 
+    @InjectMocks
     BuisnessClass buisnessClass = new BuisnessClass() ;
-    DataStore dataStore = mock(DataStore.class) ;
 
-    @BeforeEach
-    public void before(){
-        buisnessClass.setDataStore(dataStore);
-    }
+    @Mock
+    DataStore dataStore ;             // = mock(DataStore.class) ;
+
+//    @BeforeEach
+//    public void before(){
+//        buisnessClass.setDataStore(dataStore);
+//    }
 
     @Test
     public void calculateSumUsingMockito(){
@@ -41,6 +44,5 @@ public class BuisnessClassTestUsingMockito {
         when(dataStore.retrieveData()).thenReturn(new int[]{5}) ;
         assertEquals(5, buisnessClass.calculateeSum());
     }
-
 
 }
