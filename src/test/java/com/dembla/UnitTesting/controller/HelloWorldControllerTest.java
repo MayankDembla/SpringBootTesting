b9878c9994a2d7a364edf28e0b6c202dd831ccac
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-07-21 01:14:51 +0530

    Use the Status and type of content and value in mockmvc itself.

--- src/test/java/com/dembla/UnitTesting/controller/HelloWorldControllerTest.java
+++ src/test/java/com/dembla/UnitTesting/controller/HelloWorldControllerTest.java
@@ -4,15 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.web.servlet.function.RequestPredicates.accept;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloWorldController.class)
 class HelloWorldControllerTest {
@@ -27,9 +26,13 @@ class HelloWorldControllerTest {
         RequestBuilder request = MockMvcRequestBuilders
                                     .get("/hello")
                                     .accept(MediaType.APPLICATION_JSON) ;
-         MvcResult result = mockMvc.perform(request).andReturn() ;
+
+         MvcResult result = mockMvc.perform(request)
+                                   .andExpect(status().isOk())
+                                   .andExpect(content().string("Hello World"))
+                                   .andReturn() ;
 
         // verify "Hello-World"
-        assertEquals("Hello World", result.getResponse().getContentAsString());
+//        assertEquals("Hello World", result.getResponse().getContentAsString());
     }
 }
